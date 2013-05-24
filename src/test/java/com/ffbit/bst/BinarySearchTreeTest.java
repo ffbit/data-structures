888001a68eea3a5d0ad2f5007bdd6908fe82b5ba
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-05-24 11:37:38 +0300

    BST: refactoring.

--- src/test/java/com/ffbit/bst/BinarySearchTreeTest.java
+++ src/test/java/com/ffbit/bst/BinarySearchTreeTest.java
@@ -1,5 +1,6 @@
 package com.ffbit.bst;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -7,15 +8,20 @@ import static org.junit.Assert.assertThat;
 
 public class BinarySearchTreeTest {
 
+    private BinarySearchTree<Integer> bst;
+
+    @Before
+    public void setUp() throws Exception {
+        bst = new BinarySearchTree<Integer>();
+    }
+
     @Test
     public void itShouldBeEmptyWhenCreated() throws Exception {
-        BinarySearchTree bst = new BinarySearchTree();
         assertThat(bst.size(), is(0));
     }
 
     @Test
     public void itShouldNotBeEmptyWhenElementAdded() throws Exception {
-        BinarySearchTree bst = new BinarySearchTree();
         bst.add(1);
 
         assertThat(1, is(bst.size()));
