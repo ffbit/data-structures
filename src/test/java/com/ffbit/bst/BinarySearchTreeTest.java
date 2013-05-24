52c3ebce104b687669ba71261788755cdf688d10
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-05-24 17:38:41 +0300

    BST: Removing a regular node.

--- src/test/java/com/ffbit/bst/BinarySearchTreeTest.java
+++ src/test/java/com/ffbit/bst/BinarySearchTreeTest.java
@@ -105,4 +105,25 @@ public class BinarySearchTreeTest {
         assertThat(bst.size(), is(2));
     }
 
+    @Test
+    public void itShouldRemoveRegularNode() throws Exception {
+        bst.add(3);
+        bst.add(1);
+        bst.add(0);
+        bst.add(2);
+        bst.add(5);
+        bst.add(4);
+        bst.add(6);
+
+        assertThat(bst.size(), is(7));
+
+        assertThat(bst.remove(1), is(true));
+        assertThat(bst.contains(1), is(false));
+        assertThat(bst.size(), is(6));
+
+        assertThat(bst.remove(5), is(true));
+        assertThat(bst.contains(5), is(false));
+        assertThat(bst.size(), is(5));
+    }
+
 }
