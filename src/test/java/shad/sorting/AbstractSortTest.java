cd8931828c4f27d0a268d1fbac1ccd53035b2a2e
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-19 20:42:55 +0200

    Sorting - abstract parametrized test case.

---
+++ src/test/java/shad/sorting/AbstractSortTest.java
@@ -0,0 +1,42 @@
+package shad.sorting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public abstract class AbstractSortTest {
+    private Integer[] source;
+    private Integer[] expected;
+
+    public AbstractSortTest(Integer[] source, Integer[] expected) {
+        this.source = source;
+        this.expected = expected;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new Integer[]{}, new Integer[]{}},
+                {new Integer[]{1}, new Integer[]{1}},
+                {new Integer[]{2, 1}, new Integer[]{1, 2}},
+                {new Integer[]{2, 1, 1, 0}, new Integer[]{0, 1, 1, 2}},
+                {new Integer[]{1, 2, 3}, new Integer[]{1, 2, 3}},
+        });
+    }
+
+    @Test
+    public void itShouldSort() throws Exception {
+        assertThat(sorter().sort(source), is(expected));
+    }
+
+    public abstract Sorter<Integer> sorter();
+
+}
--- src/test/java/shad/sorting/SelectionSortTest.java
+++ src/test/java/shad/sorting/SelectionSortTest.java
@@ -1,42 +1,14 @@
 package shad.sorting;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-@RunWith(Parameterized.class)
-public class SelectionSortTest {
-    private Integer[] source;
-    private Integer[] expected;
+public class SelectionSortTest extends AbstractSortTest {
 
     public SelectionSortTest(Integer[] source, Integer[] expected) {
-        this.source = source;
-        this.expected = expected;
+        super(source, expected);
     }
 
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {new Integer[]{}, new Integer[]{}},
-                {new Integer[]{1}, new Integer[]{1}},
-                {new Integer[]{2, 1}, new Integer[]{1, 2}},
-                {new Integer[]{2, 1, 1, 0}, new Integer[]{0, 1, 1, 2}},
-                {new Integer[]{1, 2, 3}, new Integer[]{1, 2, 3}},
-        });
-    }
-
-    @Test
-    public void itShouldSort() throws Exception {
-        Sorter<Integer> sorter = new SelectionSort<Integer>();
-
-        assertThat(sorter.sort(source), is(expected));
+    @Override
+    public Sorter<Integer> sorter() {
+        return new SelectionSort<Integer>();
     }
 
 }
